2cb0f0a3f774d8ecd93c8687afc7f8359853fc37
Allen Zhao <allen10to11@gmail.com>
2022-08-12 16:31:21 -0700

    added processing and organization of data from individual cameras

--- scripts/avi-rip.py
+++ scripts/avi-rip.py
@@ -8,14 +8,14 @@ def getParentDir(directory):
 
 
 current_dirs_parent = getParentDir(os.getcwd())
-videos = current_dirs_parent + "\\data\\Test Plot Photos"
-export = current_dirs_parent + "\\export"
+cameras = current_dirs_parent + "\\data\\Cameras" # folder containing individual camera folders
+dest = current_dirs_parent + "\\export" # export folder
 ffmpeg_path = current_dirs_parent + "\\ffmpeg_1.4\\bin"
 skvideo.setFFmpegPath(ffmpeg_path)
 
 
 
-def getFilesWIthPaths(dir):
+def getFilesWIthPaths(dir): # get list of file paths to process
     to_process = os.listdir(dir)
     c = 0
     for i in to_process:
@@ -35,5 +35,14 @@ def aviToImage(file, output, cull=[]):
         i += 1
 
 
-for video in getFilesWIthPaths(videos):
-    aviToImage(video, export, [2])
+'''
+Get list of camera folders
+For every camera folder: grab the nth frame(s) of every video file, put it into the respective camera output folder.
+
+''' 
+for camera in getFilesWIthPaths(cameras):
+    videos = getFilesWIthPaths(camera)
+    export = dest + "\\" + os.path.basename(camera)
+    os.mkdir(export)
+    for video in videos:
+        aviToImage(video, export, [2])
