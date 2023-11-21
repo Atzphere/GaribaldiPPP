f50592f030fd7f3f24103f49f5fc1051b80b9e1c
Allen Zhao <>
2023-11-21 06:11:10 -0800

    finalize collage2 structure

--- scripts/collage2.py
+++ scripts/collage2.py
@@ -14,7 +14,7 @@ scale = 3.0
 IMAGE_WIDTH = int(160 * scale)
 IMAGE_HEIGHT = int(90 * scale)
 
-cam_folder = dt.get_subdirs("/home/azhao/projects/def-nbl/Garibaldi_Lake_shared/working_directories/azhao_pheno_processing_workingdir/2022_processed_photos/export_all_photos_v3", fullpath=True)
+cam_folder = dt.get_subdirs("/home/azhao/projects/def-nbl/Garibaldi_Lake_shared/working_directories/azhao_pheno_processing_workingdir/2023_processed_photos/export_all_photos", fullpath=True)
 exp_folder = "/home/azhao/projects/def-nbl/Garibaldi_Lake_shared/working_directories/azhao_pheno_processing_workingdir/GaribaldiPPP/scripts/collage_2023/"
 
 def get_timestamp(imgname):
